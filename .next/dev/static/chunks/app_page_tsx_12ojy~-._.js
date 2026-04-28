55ef4bd9c7bb09a0d43bc3e65653a3c2491ec82a
Nicolás Giraldez <klaus.15@gmail.com>
2026-04-28 11:14:56 -0300

    chore: generate Next.js development and server build artifacts

--- .next/dev/static/chunks/app_page_tsx_12ojy~-._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_0njfro0._.js","static/chunks/_0z_oj47._.js"],
-    source: "dynamic"
-});
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"x0P6kuhAqvqSy/Rb5ZvxB83Q/Q1B3GrEDWFzFkKr++U=\"\n}"
